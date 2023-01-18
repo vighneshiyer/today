4f6af9cf0830bc9db696052bd5c6967f0243d5a9
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2023-01-17 16:53:39 -0800

    parse basic tasks

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,28 +2,31 @@ import mistune
 from mistune import BlockState
 from mistune.renderers.markdown import MarkdownRenderer
 
+from today.task import parse_markdown, Task
+
 
 class TestParser:
     simple_md = """# Tasks
 
 - [ ] Task 1
+
+- [ ] Task 2
 """
+    ast_parser = mistune.create_markdown(renderer=None)
 
     def test_roundtrip(self) -> None:
         # Convert Markdown to AST and back to Markdown
-        ast_parser = mistune.create_markdown(renderer=None)
-        ast = ast_parser(self.simple_md)
+        ast = self.ast_parser(self.simple_md)
 
         md_renderer = MarkdownRenderer()
         md = md_renderer(ast, BlockState())
-        print(md)
         assert md == self.simple_md
 
-    """
-    def test_basic_tasks(self) -> None:
-        with ASTRenderer() as renderer:
-            doc = Document(self.simple_md)
-            #print(doc.children[0].children)
-            rendered = renderer.render(doc)  # render the AST
-            print(rendered)
-    """
+    def test_basic_task_parsing(self) -> None:
+        # Parse an AST to tasks
+        ast = self.ast_parser(self.simple_md)
+        tasks = parse_markdown(ast)
+        assert tasks == [
+            Task(path=["Tasks"], title="Task 1", done=False),
+            Task(path=["Tasks"], title="Task 2", done=False)
+        ]
--- today/task.py
+++ today/task.py
@@ -1,18 +1,55 @@
-from typing import Optional, List
-from dataclasses import dataclass
+from typing import Optional, List, Any
+from dataclasses import dataclass, field
 from datetime import date
 
 
-@dataclass(frozen=True)
+@dataclass
 class Task:
     path: List[str]
     title: str
-    created_date: Optional[date]
-    reminder_date: Optional[date]
-    due_date: Optional[date]
-    finished_date: Optional[date]
     done: bool
-    subtasks: List["Task"]
+    subtasks: List["Task"] = field(default_factory=lambda: [])
+    created_date: Optional[date] = None
+    reminder_date: Optional[date] = None
+    due_date: Optional[date] = None
+    finished_date: Optional[date] = None
 
-def parse_markdown() -> List[Task]:
-    pass
+
+def parse_task_title(headings_stack: List[str], title: str) -> Task:
+    assert title.startswith("[ ]")
+    return Task(path=headings_stack, title=title[4:], done=False)
+
+
+def parse_markdown(md: List[Any]) -> List[Task]:
+    headings_stack: List[str] = []
+    current_task: Optional[Task] = None
+    tasks: List[Task] = []
+
+    for item in md:
+        if item['type'] == "heading":
+            headings_stack.append(item['children'][0]['raw'])
+        elif item['type'] == "blank_line":
+            continue
+        elif item['type'] == "list":
+            list_items = item['children']
+            for list_item in list_items:
+                assert len(list_item['children']) == 1
+                text_ast = list_item['children'][0]
+
+                assert text_ast['type'] == 'paragraph'
+                list_item_text = "".join([child['raw'] for child in text_ast['children']])
+
+                if list_item_text.startswith("[ ]"):  # This the start of a new task
+                    if current_task is not None:  # Another task was being parsed
+                        tasks.append(current_task)
+                        current_task = parse_task_title(headings_stack, list_item_text)
+                    else:  # No task has been seen yet
+                        current_task = parse_task_title(headings_stack, list_item_text)
+        else:
+            print(item)
+            assert False
+
+    if current_task is not None:
+        tasks.append(current_task)
+
+    return tasks
